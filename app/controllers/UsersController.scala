9fb7cab0f06bf478c1c384b76a56167634180f28
Alessandro Maccagnan <alessandro.maccagnan@gmail.com>
2013-06-27 01:19:01 +0200

    + small api to test things

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -3,7 +3,8 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json.Json
-
+import views.html.defaultpages.badRequest
+import play.api.libs.json.JsValue
 
 object UsersController extends Controller {
 
@@ -24,10 +25,71 @@ object UsersController extends Controller {
   //    
   //    }
 
-  def show(id: Long) = Action { request =>
-      Ok(Json.toJson(
-        Map("status" -> "OK", "message" -> ("Hello " + "name"))))
+  def update(id: Long) = Action {
+    request =>
+      val myId: Long = id
+      val body: AnyContent = request.body
+      val jsonBody: Option[JsValue] = body.asJson
+      val raw = body.asRaw
+      jsonBody.map { value =>
+        Ok(Json.toJson(
+          Map("status" -> "success",
+            "data" -> (Json.stringify(value)))))
+      }.getOrElse {
+        Ok(Json.toJson(
+          Map("status" -> "fail", "message" -> ("BadRequest"))))
+      }
+
   }
 
+  def show(id: Long) = Action { request =>
+    Ok(Json.toJson(
+      Map("status" -> "OK", "message" -> ("Hello " + "name"))))
+  }
+
+  parse.json
+
+  def create() = Action {
+    request =>
+      val body: AnyContent = request.body
+      val jsonBody: Option[JsValue] = body.asJson
+      val raw = body.asRaw
+      jsonBody.map { value =>
+        Ok(Json.toJson(
+          Map("status" -> "success",
+            "data" -> (Json.stringify(value)))))
+      }.getOrElse {
+        Ok(Json.toJson(
+          Map("status" -> "fail", "message" -> ("BadRequest"))))
+      }
+
+  }
+
+  //  def sayHello = Action(parse.json) { request =>
+  //  (request.body \ "name").asOpt[String].map { name =>
+  //    Ok(toJson(
+  //      Map("status" -> "OK", "message" -> ("Hello " + name))
+  //    ))
+  //  }.getOrElse {
+  //    BadRequest(toJson(
+  //      Map("status" -> "KO", "message" -> "Missing parameter [name]")
+  //    ))
+  //  }
+  //}
+
+  //  def create() = Action {
+  //    request =>
+  //      val body: AnyContent = request.body
+  //      val jsonBody: Option[JsValue] = body.asJson
+  //
+  //      jsonBody.map { value =>
+  //        Ok(Json.toJson(
+  //          Map("status" -> "success", "data" -> (Json.stringify(value)))))
+  //      }.getOrElse {
+  //        Ok(Json.toJson(
+  //          Map("status" -> "fail", "message" -> ("BadRequest"))))
+  //      }
+  //
+  //  }
 
 }
