c1dedd5571515f6d0b55ae3c2e39a2afc12648d1
miloertola <miloertola@gmail.com>
2013-06-28 03:10:36 +0200

    test fork update

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-      sbt.Keys.fork in Test := false
+//      sbt.Keys.fork in Test := false
   )
 
 }
