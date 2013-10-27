5679aafc15e5cc7404fffaf6ba93b25abb841d98
Alessandro Maccagnan <alessandro.maccagnan@gmail.com>
2013-10-27 12:51:12 +0100

    + revert migration

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,23 +2,16 @@
  
 # --- !Ups
 
+CREATE SEQUENCE my_user_id_seq;
 CREATE TABLE my_user (
-    id serial NOT NULL,
-    username character varying(255) NOT NULL,
+    id integer NOT NULL DEFAULT nextval('my_user_id_seq'),
+    username varchar(255),
     lat varchar(255),
     lngt varchar(255),
-    deleted boolean,
-    CONSTRAINT my_user_pkey PRIMARY KEY (id)
+    deleted boolean
 );
  
-CREATE INDEX index_user_on_username
-  ON my_user
-  USING btree
-  (username COLLATE pg_catalog."default");
-
-
 # --- !Downs
  
 DROP TABLE my_user;
-DROP INDEX my_user_id_seq;
-
+DROP SEQUENCE my_user_id_seq;
